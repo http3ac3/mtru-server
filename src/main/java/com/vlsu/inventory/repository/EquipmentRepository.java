15e318d71351eaa1a031f2f6c9ef3106ae23fd4e
yurcheshka <ura19092004@gmail.com>
2024-03-31 14:15:57 +0300

    add pagination to all equipment repository methods

--- src/main/java/com/vlsu/inventory/repository/EquipmentRepository.java
+++ src/main/java/com/vlsu/inventory/repository/EquipmentRepository.java
@@ -1,6 +1,8 @@
 package com.vlsu.inventory.repository;
 
 import com.vlsu.inventory.model.Equipment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -9,15 +11,14 @@ import java.util.Optional;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
     Optional<Equipment> findByInventoryNumber(String inventoryNumber);
-    List<Equipment> findByInventoryNumberContaining(String inventoryNumber);
-    List<Equipment> findByName(String name);
-    List<Equipment> findByNameContaining(String name);
-    List<Equipment> findByCommissioningDate(LocalDate commissioningDate);
-    List<Equipment> findByCommissioningActNumber(String commissioningActNumber);
-    List<Equipment> findByDecommissioningDate(LocalDate decommissioningDate);
-    List<Equipment> findByDecommissioningActNumber(String decommissioningActNumber);
-    List<Equipment> findByResponsibleId(Long responsibleId);
-    List<Equipment> findByPlacementId(Long placementId);
-    List<Equipment> findBySubcategoryId(Long subcategoryId);
-
+    Page<Equipment> findByInventoryNumberContaining(String inventoryNumber, Pageable pageable);
+    Page<Equipment> findByName(String name, Pageable pageable);
+    Page<Equipment> findByNameContaining(String name, Pageable pageable);
+    Page<Equipment> findByCommissioningDate(LocalDate commissioningDate, Pageable pageable);
+    Page<Equipment> findByCommissioningActNumber(String commissioningActNumber, Pageable pageable);
+    Page<Equipment> findByDecommissioningDate(LocalDate decommissioningDate, Pageable pageable);
+    Page<Equipment> findByDecommissioningActNumber(String decommissioningActNumber, Pageable pageable);
+    Page<Equipment> findByResponsibleId(Long responsibleId, Pageable pageable);
+    Page<Equipment> findByPlacementId(Long placementId, Pageable pageable);
+    Page<Equipment> findBySubcategoryId(Long subcategoryId, Pageable pageable);
 }
