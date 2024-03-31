d0ab050e876f393bc85b2b708e6beb3c8338fae3
yurcheshka <ura19092004@gmail.com>
2024-03-31 12:32:02 +0300

    create subcategory service, set show-sql on jpa to true

---
+++ src/main/java/com/vlsu/inventory/service/SubcategoryService.java
@@ -0,0 +1,47 @@
+package com.vlsu.inventory.service;
+
+import com.vlsu.inventory.model.Subcategory;
+import com.vlsu.inventory.repository.CategoryRepository;
+import com.vlsu.inventory.repository.SubcategoryRepository;
+import com.vlsu.inventory.util.exception.ResourceHasDependenciesException;
+import com.vlsu.inventory.util.exception.ResourceNotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubcategoryService {
+    private final SubcategoryRepository subcategoryRepository;
+
+    private final CategoryRepository categoryRepository;
+    public SubcategoryService(SubcategoryRepository subcategoryRepository, CategoryRepository categoryRepository) {
+        this.subcategoryRepository = subcategoryRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Subcategory> getAllSubcategories() {
+        return subcategoryRepository.findAll();
+    }
+
+    public void createSubcategory(Subcategory subcategory) {
+        subcategoryRepository.save(subcategory);
+    }
+
+    public void updateSubcategoryById(Long id, Subcategory subcategory) throws ResourceNotFoundException {
+        Subcategory subcategoryToUpdate = subcategoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Subcategory with id: " + id + " not found"));
+        subcategoryToUpdate.setName(subcategory.getName());
+        subcategoryToUpdate.setCategory(subcategory.getCategory());
+        subcategoryRepository.save(subcategoryToUpdate);
+    }
+
+    public void deleteSubcategoryById(Long id) throws ResourceNotFoundException, ResourceHasDependenciesException {
+        Subcategory subcategoryToDelete = subcategoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Subcategory with id: " + id + " not found"));
+        if (!subcategoryToDelete.getEquipment().isEmpty()) {
+            throw new ResourceHasDependenciesException("Category with id: " + id + " has relations with equipment");
+        }
+        subcategoryRepository.deleteById(id);
+    }
+}
