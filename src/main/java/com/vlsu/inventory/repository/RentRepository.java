22819280fee1157024b0a9a494efd25eea55b03e
yurcheshka <ura19092004@gmail.com>
2024-04-05 13:47:06 +0300

    fix: methods now return list instead page, replace date methods to specifications

--- src/main/java/com/vlsu/inventory/repository/RentRepository.java
+++ src/main/java/com/vlsu/inventory/repository/RentRepository.java
@@ -1,24 +1,31 @@
 package com.vlsu.inventory.repository;
 
 import com.vlsu.inventory.model.Rent;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public interface RentRepository extends JpaRepository<Rent, Long> {
-    Page<Rent> findByCreateDate(LocalDate createDate, Pageable pageable);
-    Page<Rent> findByEndDate(LocalDate date, Pageable pageable);
-    Page<Rent> findByCreateDateBetween(LocalDate fromCreateDate, LocalDate toCreateDate, Pageable pageable);
-    Page<Rent> findByEndDateBetween(LocalDate fromEndDate, LocalDate toEndDate, Pageable pageable);
-    Page<Rent> findByCreateDateBefore(LocalDate beforeCreateDate, Pageable pageable);
-    Page<Rent> findByCreateDateAfter(LocalDate afterCreateDate, Pageable pageable);
-    Page<Rent> findByEndDateBefore(LocalDate beforeEndDate, Pageable pageable);
-    Page<Rent> findByEndDateAfter(LocalDate afterEndDate, Pageable pageable);
-    Page<Rent> findByEquipmentId(Long equipmentId, Pageable pageable);
-    Page<Rent> findByResponsibleId(Long responsibleId, Pageable pageable);
-    Page<Rent> findByPlacementId(Long placementId, Pageable pageable);
+public interface RentRepository extends JpaRepository<Rent, Long>, JpaSpecificationExecutor<Rent> {
+    static Specification<Rent> createDateTimeFrom(LocalDateTime dateTime) {
+        return (rent, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(rent.get("createDate"), dateTime);
+    }
+    static Specification<Rent> createDateTimeTo(LocalDateTime dateTime) {
+        return (rent, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(rent.get("createDate"), dateTime);
+    }
+
+    static Specification<Rent> endDateTimeFrom(LocalDateTime dateTime) {
+        return (rent, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(rent.get("endDate"), dateTime);
+    }
+
+    static Specification<Rent> endDateTimeTo(LocalDateTime dateTime) {
+        return (rent, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(rent.get("endDate"), dateTime);
+    }
+    List<Rent> findByEquipmentId(Long equipmentId);
+    List<Rent> findByResponsibleId(Long responsibleId);
+    List<Rent> findByPlacementId(Long placementId);
     void deleteByEquipmentId(Long equipmentId);
 }
